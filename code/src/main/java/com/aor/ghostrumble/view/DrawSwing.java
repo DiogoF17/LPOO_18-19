24e0dc8735980f49c0c3c35c9dd3a9ef67971d51
EduRibeiro00 <eribeiro306@gmail.com>
2019-05-21 10:57:53 +0100

    Start of swing

---
+++ code/src/main/java/com/aor/ghostrumble/view/DrawSwing.java
@@ -0,0 +1,13 @@
+package com.aor.ghostrumble.view;
+
+import com.aor.ghostrumble.model.HauntedHouse;
+
+import java.io.IOException;
+
+public class DrawSwing implements DrawingMethod {
+
+    @Override
+    public void drawAll(HauntedHouse house) throws IOException {
+
+    }
+}
---
+++ code/src/main/java/com/aor/ghostrumble/view/GameSwing.java
@@ -0,0 +1,8 @@
+package com.aor.ghostrumble.view;
+
+import com.aor.ghostrumble.Game;
+
+public class GameSwing extends Game {
+
+
+}
