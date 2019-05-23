27673d5300e42f2e480f62171b1f80d9bd230d6c
diogosmac <diogo.smac@gmail.com>
2019-05-23 14:54:29 +0100

    Pick'n'choose

--- code/src/main/java/com/aor/ghostrumble/Application.java
+++ code/src/main/java/com/aor/ghostrumble/Application.java
@@ -4,17 +4,51 @@ import com.aor.ghostrumble.view.GameLanterna;
 import com.aor.ghostrumble.view.GameSwing;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Application {
+
+    private static int getGameType() {
+
+        Scanner scan = new Scanner(System.in);
+        int i;
+
+        do {
+
+            System.out.println("Choose your version: ");
+            System.out.println();
+            System.out.println("0: Retro");
+            System.out.println("1: Modern");
+            System.out.print("Option: ");
+            i = scan.nextInt();
+            System.out.println();
+
+            if (i < 0 || i > 1) {
+                System.out.println("Invalid option! Choose again.");
+                System.out.println();
+            }
+
+        } while (i < 0 || i > 1);
+
+        return i;
+    }
 
     public static void main(String[] args) {
 
         try {
-            // Game game = new GameLanterna();
-            Game game = new GameSwing();
+            Game game;
+
+            if (getGameType() == 0)
+                game = new GameLanterna();
+            else {
+                game = new GameSwing();
+            }
+
             game.run();
-        }catch(IOException e) {
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 }
