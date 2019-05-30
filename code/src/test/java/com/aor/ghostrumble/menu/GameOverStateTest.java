7ee6f0db1ae6e71bcaca4c74b9f8699462ce5074
EduRibeiro00 <eribeiro306@gmail.com>
2019-05-30 15:00:06 +0100

    Tudo testado em factory e menu

--- code/src/test/java/com/aor/ghostrumble/menu/GameOverStateTest.java
+++ code/src/test/java/com/aor/ghostrumble/menu/GameOverStateTest.java
@@ -1,4 +1,82 @@
 package com.aor.ghostrumble.menu;
 
+import com.aor.ghostrumble.StateObserver;
+import com.aor.ghostrumble.factory.ViewAbstractFactory;
+import com.aor.ghostrumble.factory.ViewSwingFactory;
+import com.aor.ghostrumble.menu.event.MenuEvent;
+import com.aor.ghostrumble.menu.model.MenuModel;
+import com.aor.ghostrumble.menu.view.ViewMenu;
+import com.aor.ghostrumble.play.GameState;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.ArgumentMatchers.any;
+
 public class GameOverStateTest {
+
+    private GameOverState state;
+
+    @Before
+    public void initGameOverState() {
+        ViewAbstractFactory fac = new ViewSwingFactory();
+        state = new GameOverState(fac, 0);
+    }
+
+    @Test
+    public void testUpdatePrepareStateChange() {
+        MenuModel model = Mockito.mock(MenuModel.class);
+        state.setModel(model);
+        ViewMenu view = Mockito.mock(ViewMenu.class);
+        state.setView(view);
+        MenuEvent event = Mockito.mock(MenuEvent.class);
+        Mockito.when(event.process(model)).thenReturn(true);
+        Mockito.when(view.getEvent()).thenReturn(event);
+        StateObserver observer = Mockito.mock(StateObserver.class);
+        state.setObserver(observer);
+
+        state.update();
+
+        Mockito.verify(view, Mockito.times(1)).prepareStateChange();
+    }
+
+
+    @Test
+    public void testUpdateSwitchToGameState() {
+        MenuModel model = Mockito.mock(MenuModel.class);
+        state.setModel(model);
+        ViewMenu view = Mockito.mock(ViewMenu.class);
+        state.setView(view);
+        MenuEvent event = Mockito.mock(MenuEvent.class);
+        Mockito.when(event.process(model)).thenReturn(true);
+        Mockito.when(view.getEvent()).thenReturn(event);
+        Mockito.when(model.willPlay()).thenReturn(true);
+        StateObserver observer = Mockito.mock(StateObserver.class);
+        state.setObserver(observer);
+
+        state.update();
+
+        Mockito.verify(observer, Mockito.times(1)).changeState(any(GameState.class));
+    }
+
+
+    @Test
+    public void testUpdateSwitchToMainMenu() {
+        MenuModel model = Mockito.mock(MenuModel.class);
+        ViewMenu view = Mockito.mock(ViewMenu.class);
+        state.setView(view);
+        MenuEvent event = Mockito.mock(MenuEvent.class);
+        Mockito.when(event.process(model)).thenReturn(true);
+        Mockito.when(view.getEvent()).thenReturn(event);
+        Mockito.when(model.willPlay()).thenReturn(false);
+        state.setModel(model);
+        StateObserver observer = Mockito.mock(StateObserver.class);
+        state.setObserver(observer);
+
+        state.update();
+
+        Mockito.verify(observer, Mockito.times(1)).changeState(any(MainMenuState.class));
+    }
+
+
 }
--- code/src/test/java/com/aor/ghostrumble/menu/MainMenuStateTest.java
+++ code/src/test/java/com/aor/ghostrumble/menu/MainMenuStateTest.java
@@ -1,15 +1,19 @@
 package com.aor.ghostrumble.menu;
 
+import com.aor.ghostrumble.StateObserver;
 import com.aor.ghostrumble.factory.ViewAbstractFactory;
 import com.aor.ghostrumble.factory.ViewSwingFactory;
 import com.aor.ghostrumble.menu.event.MenuEvent;
 import com.aor.ghostrumble.menu.model.MenuModel;
 import com.aor.ghostrumble.menu.view.ViewMenu;
+import com.aor.ghostrumble.play.GameState;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 
 public class MainMenuStateTest {
 
@@ -28,14 +32,54 @@ public class MainMenuStateTest {
     }
 
     @Test
-    public void testUpdate() {
+    public void testUpdatePrepareStateChange() {
         MenuModel model = Mockito.mock(MenuModel.class);
         state.setModel(model);
         ViewMenu view = Mockito.mock(ViewMenu.class);
         state.setView(view);
         MenuEvent event = Mockito.mock(MenuEvent.class);
         Mockito.when(event.process(model)).thenReturn(true);
+        Mockito.when(view.getEvent()).thenReturn(event);
 
-        // continuar
+        state.update();
+
+        Mockito.verify(view, Mockito.times(1)).prepareStateChange();
     }
+
+    @Test
+    public void testUpdateSwitchToGameState() {
+        MenuModel model = Mockito.mock(MenuModel.class);
+        state.setModel(model);
+        ViewMenu view = Mockito.mock(ViewMenu.class);
+        state.setView(view);
+        MenuEvent event = Mockito.mock(MenuEvent.class);
+        Mockito.when(event.process(model)).thenReturn(true);
+        Mockito.when(view.getEvent()).thenReturn(event);
+        Mockito.when(model.willPlay()).thenReturn(true);
+        StateObserver observer = Mockito.mock(StateObserver.class);
+        state.setObserver(observer);
+
+        state.update();
+
+        Mockito.verify(observer, Mockito.times(1)).changeState(any(GameState.class));
+    }
+
+
+    @Test
+    public void testUpdateEndGame() {
+        MenuModel model = Mockito.mock(MenuModel.class);
+        ViewMenu view = Mockito.mock(ViewMenu.class);
+        state.setView(view);
+        MenuEvent event = Mockito.mock(MenuEvent.class);
+        Mockito.when(event.process(model)).thenReturn(true);
+        Mockito.when(view.getEvent()).thenReturn(event);
+        Mockito.when(model.willPlay()).thenReturn(false);
+        state.setModel(model);
+
+        state.update();
+
+        assertFalse(state.keepGoing());
+    }
+
+
 }
